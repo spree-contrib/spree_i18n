be988f279b76d07e119e8b2d7d90837023c076b1
Tsutomu Kuroda <t-kuroda@oiax.jp>
2012-04-26 14:39:59 +0900

    Allow users to split translation files into a separate directory.
    A configuration option i18n.available_locales is introduced
    to allow users to load only selected locales.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ require 'rspec'
 require 'i18n-spec'
 require 'i18n/core_ext/hash'
 require 'active_support/core_ext/kernel/reporting'
+require 'support/fake_app'
 
 RSpec.configure do |config|
   config.mock_with :rspec
