279878d639888d119cc8175bcf7702da5bd879ec
Alexander Nеgоdа <alexander.negoda@gmail.com>
2013-08-27 18:08:13 +0400

    Update install_generator.rb
    Added options[:auto_run_migrations] to run_migrations method

--- lib/generators/spree_i18n/install/install_generator.rb
+++ lib/generators/spree_i18n/install/install_generator.rb
@@ -16,12 +16,12 @@ module SpreeI18n
       end
 
       def run_migrations
-         res = ask "Would you like to run the migrations now? [Y/n]"
-         if res == "" || res.downcase == "y"
-           run 'rake db:migrate'
-         else
-           puts "Skiping rake db:migrate, don't forget to run it!"
-         end
+        run_migrations = options[:auto_run_migrations] || ['', 'y', 'Y'].include?(ask 'Would you like to run the migrations now? [Y/n]')
+        if run_migrations
+          run 'bundle exec rake db:migrate'
+        else
+          puts 'Skipping rake db:migrate, don\'t forget to run it!'
+        end
       end
     end
   end
