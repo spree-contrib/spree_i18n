75d3d831d1f05a29f903d16842d716769d767ee6
Washington Luiz <huoxito@gmail.com>
2015-03-23 23:40:42 -0300

    Let ransack know about translations
    Since globalize 5 won't store translated data on default tables we need
    to tweak the table / attribute name to let it fetch the data on the
    right place.
    
    This is just a proof of concept on what we could do to
    improve the spree / ransack / globalize love here. Not sure this will
    work with associations yet or even if it will work at all in all
    scenarios

--- app/models/spree_i18n/translatable.rb
+++ app/models/spree_i18n/translatable.rb
@@ -4,5 +4,22 @@ module SpreeI18n
     included do
       accepts_nested_attributes_for :translations
     end
+
+    class_methods do
+      def ransack(params = {}, options = {})
+        names = params.keys
+
+        names.each do |n|
+          translated_attribute_names.each do |t|
+            if n.to_s.starts_with? t.to_s
+              params[:"translations_#{n}"] = params[n]
+              params.delete n
+            end
+          end
+        end
+
+        super(params, options)
+      end
+    end
   end
 end
