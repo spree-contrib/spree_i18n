26afc7296d932f09dc580fd62e814d8e6ea2cf2a
Matijs van Zuijlen <matijs@matijs.net>
2015-07-25 15:59:45 +0200

    Ensure RansackTranslator is available

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -1,7 +1,9 @@
+require 'spree_i18n/ransack_translator'
+
 module Spree
   Variant.class_eval do
     def self.ransack(params = {}, options = {})
-      translated_params = RansackTranslator.new(self, params).translated_params
+      translated_params = SpreeI18n::RansackTranslator.new(self, params).translated_params
 
       super(translated_params, options)
     end
