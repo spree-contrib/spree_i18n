1a2791cedd1d06f45e5976737942049aea3dadd4
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2015-08-27 10:59:36 -0400

    Update whitelist usage.

--- app/models/spree_i18n/translatable.rb
+++ app/models/spree_i18n/translatable.rb
@@ -4,9 +4,9 @@ module SpreeI18n
   module Translatable
     extend ActiveSupport::Concern
 
-    included do
+    included do |klass|
       accepts_nested_attributes_for :translations
-      whitelisted_ransackable_associations |= ['translations']
+      klass.whitelisted_ransackable_associations |= ['translations']
     end
 
     class_methods do
