86b7da65409f3a5f1520f320978e086af5835600
Bruno Cavalcante <bruno.cavalcante@gocase.com.br>
2017-03-07 17:21:34 -0300

    Not using routing-filter gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Spree::Core::Engine.add_routes do
   # from routing-filter gem
-  filter :locale
+  #filter :locale
 
   get '/locales', to: 'locale#index', as: :locales
   post '/locale/set', to: 'locale#set', defaults: { format: :json }, as: :set_locale
