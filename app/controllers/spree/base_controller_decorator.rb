9b54d555448013e4fdd0d0133c13e98e5e522ef8
PSzyszkaSpark <pawel.szyszka@sparksolutions.co>
2019-10-01 13:15:52 +0200

    Small fix for base controller decorators

--- app/controllers/spree/base_controller_decorator.rb
+++ app/controllers/spree/base_controller_decorator.rb
@@ -1,4 +1,4 @@
 module Spree::BaseControllerDecorator
-  Spree::BaseController.include(SpreeI18n::LocaleHelper)
+  Spree::BaseController.include(SpreeI18n::ControllerLocaleHelper)
 end
 Spree::BaseController.prepend(Spree::BaseControllerDecorator)
