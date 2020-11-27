e3433f436657f1758b0175b1c4ca7c3e5cb4238e
ghaithdabash <ghaithdabash@gmail.com>
2020-11-27 20:30:51 +0200

    Amendments for spree 4.1

--- app/assets/javascripts/spree/frontend/locale.js
+++ app/assets/javascripts/spree/frontend/locale.js
@@ -2,12 +2,12 @@ Spree.fetch_locales = function () {
   return $.ajax({
     url: Spree.pathFor('locales')
   }).done(function (data) {
-    $('#main-nav-bar > .navbar-right').append(data);
+    $('#locales').html(data);
   });
 };
 
 $(function () {
-  $('body').on('change', '#locale-select select', function () {
+  $(document).on('change', '#locale-select select', function () {
     $(this).parents('form').submit();
   });
 });
