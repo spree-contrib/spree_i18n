483844d62985bd0d2dede68198abb6fa65d4b543
ghaithdabash <ghaithdabash@gmail.com>
2020-12-04 17:05:49 +0200

    Revert "Modify adding fetch_locales()"
    This reverts commit 1229cb3e92e7f168f8ad0807111f8f0a3e5f3330.

--- app/overrides/spree/adds_locales_bar_to_main_nav_bar.rb
+++ app/overrides/spree/adds_locales_bar_to_main_nav_bar.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
   virtual_path: 'spree/shared/_footer',
   name: 'main_nav_bar_locales_bar',
-  insert_bottom: '.footer-spree-menu-links',
+  insert_bottom: '#locales',
   partial: 'spree/shared/locales_bar'
 )
